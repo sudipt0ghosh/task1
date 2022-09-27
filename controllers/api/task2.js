7afd33fffa72509b08c40ce7578123a366e22de3
sghoshte <sudghosh@technoexponent.com>
2022-09-27 10:45:27 +0530

    totalPages add in task2

--- controllers/api/task2.js
+++ controllers/api/task2.js
@@ -28,17 +28,27 @@ exports.list = async function (req, res) {
               ],
             },
           ],
-          order: [["name", "ASC"]],
+          // order: [["name", "ASC"]],
           offset: (page - 1) * perPage,
           limit: perPage,
         })
         .then(function (resData) {
+          let totalPage = () => {
+            let quotient = dataCount / perPage;
+            let remainder = dataCount % perPage;
+            if (remainder > 0) {
+              return Math.floor(quotient) + 1;
+            } else {
+              return Math.floor(quotient);
+            }
+          };
           res.status(200).json({
             message: "USER Listing",
             data: resData,
             perpage: perPage,
-            pageno: page,
-            totalcount: dataCount,
+            pageno: parseInt(page),
+            totalPages: totalPage(),
+            totalDataCount: dataCount,
           });
         });
     });
